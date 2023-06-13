cf7eecc0919550d56296f8cef4fc95885d38cc9c
Wenfang Du <no_reply_mail@163.com>
2023-06-13 13:52:12 +0800

    chore!: bumped deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Comp from './Comp.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,7 @@ function App() {
         </p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <Comp name='foo'></Comp>
     </div>
   )
 }
---
+++ src/Comp.tsx
@@ -0,0 +1,10 @@
+export default function ({ name }) {
+  return (
+    <div>
+      <em>{name}</em>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptate et laborum tempore
+      amet. Optio amet quos, repellendus cupiditate tempore, deserunt ipsam recusandae alias omnis
+      placeat libero dolores, necessitatibus ipsum.
+    </div>
+  )
+}
